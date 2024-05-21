b32a618b977bb4887e6e2eee0ea262a839972e2b
abolfazl <afgboy2922@gmail.com>
2024-05-21 17:54:02 +0330

    change config

--- core/config.php
+++ core/config.php
@@ -4,6 +4,5 @@ const USERNAME = "root";
 const PASSWORD = "";
 const DATABASE = "project1";
 const SERVER_NAME = "localhost";
-const URL = "http://localhost/projee";
-// const URL = "http://Farawin-messanger.test/";
+const URL = "http://localhost/project1";
 ?>
