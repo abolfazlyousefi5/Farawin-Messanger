ba426ca47a687c38ffea2401054363066b933afb
abolfazl <afgboy2922@gmail.com>
2024-06-05 13:41:31 +0330

    add image

--- app/views/index/index.php
+++ app/views/index/index.php
@@ -31,7 +31,7 @@
 							<li class="active">
 								<div class="d-flex bd-highlight">
 									<div class="img_cont">
-										<img src="https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg" class="rounded-circle user_img">
+										<img src="public/images/default-profile.png" class="rounded-circle user_img">
 										<span class="online_icon"></span>
 									</div>
 									<div class="user_info">
